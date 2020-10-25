a430cd14d16626f19c484ee31629bfde0308cd7e
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2020-10-25 11:13:29 +0100

    Listing inputs, plug/unplug update

--- src/MIDI_Communicator.ts
+++ src/MIDI_Communicator.ts
@@ -1,24 +1,56 @@
 export default class MIDI_Communicator {
     private midiAccess: WebMidi.MIDIAccess | null;
+    private midiInputSelectElem: HTMLSelectElement | null;
 
     constructor() {
         this.midiAccess = null;
+        this.midiInputSelectElem = null;
     }
 
-    public async requestAccess() {
+    public async init(midiInputSelectElem: HTMLSelectElement) {
         try {
             this.midiAccess = await navigator.requestMIDIAccess();
             console.log('MIDI access gained');
+
+            this.midiInputSelectElem = midiInputSelectElem;
+
+            this.updateInputSelect();
+
+            this.midiAccess.onstatechange = this.onstatechange;
         } catch(err) {
             console.error('MIDI access rejected ' + err);
         }
     }
 
-    public getInputs(): WebMidi.MIDIInputMap | never {
+    public getInputs = (): WebMidi.MIDIInputMap | never => {
         if (!this.midiAccess) {
             throw new Error('No MIDI access');
         } else {
             return this.midiAccess.inputs;
         }
     }
+
+    private onstatechange = (e: WebMidi.MIDIConnectionEvent): void => {
+        this.updateInputSelect();
+    }
+
+    private updateInputSelect = (): void => {
+        for (let i = this.midiInputSelectElem!.size; i >= 0; --i) {
+            this.midiInputSelectElem!.remove(i);
+        }
+
+        for (let input of Array.from(this.getInputs().values())) {
+            let option = document.createElement('option');
+    
+            if (input.name)
+                option.text = input.name;
+            else
+                option.text = input.id;
+            
+            option.value = input.id;
+            
+            this.midiInputSelectElem!.add(option);
+        }
+    }
+    
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
 import MIDI_Communicator from './MIDI_Communicator';
 
-const midiInputSelectElem = document.getElementById('MIDI_Input_sel') as HTMLInputElement;
+const midiInputSelectElem = document.getElementById('MIDI_Input_sel') as HTMLSelectElement;
 
 const midiCommunicator = new MIDI_Communicator();
 
-midiCommunicator.requestAccess().then(
-    () => {
-        for (let input in midiCommunicator.getInputs()) {
-
-        }
-    }
-);
-
+midiCommunicator.init(midiInputSelectElem);
