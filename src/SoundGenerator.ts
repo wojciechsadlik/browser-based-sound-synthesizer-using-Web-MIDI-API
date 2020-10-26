c02fc65f0f1560cadae5d553383a01ec9e307ff7
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2020-10-26 08:44:03 +0100

    noteOn noteOff generateSound methods

--- src/SoundGenerator.ts
+++ src/SoundGenerator.ts
@@ -1,21 +1,46 @@
 export default class SoundGenerator {
     private context: AudioContext;
-    private oscillators: OscillatorNode[];
+    private oscillators: Map<number, OscillatorNode>;
     private masterGainNode: GainNode | null;
+    private selectedWaveType: OscillatorType | null;
 
     constructor() {
         this.context = new window.AudioContext();
-        this.oscillators = [];
+        this.oscillators = new Map<number, OscillatorNode>();
         this.masterGainNode = this.context.createGain();
+        this.selectedWaveType = null;
     }
 
     
     public noteOn = (noteNumber: number): void => {
-
+        let osc = this.generateSound(this.noteNumberToFrequency(noteNumber));
+        
+        this.oscillators.set(noteNumber, osc);
     }
 
     public noteOff = (noteNumber: number): void => {
+        let activeOscillator = this.oscillators.get(noteNumber);
+        
+        if (typeof activeOscillator !== 'undefined') {
+            activeOscillator.stop();
+        }
 
+        this.oscillators.delete(noteNumber);
+    }
+
+    private generateSound = (frequency: number): OscillatorNode => {
+        let osc = this.context.createOscillator();
+        osc.connect(this.masterGainNode as AudioNode);
+
+        if (this.selectedWaveType) {
+            osc.type = this.selectedWaveType;
+        }
+
+        osc.frequency.value = frequency;
+
+        osc.start();
+
+        return osc;
     }
 
     private noteNumberToFrequency = (noteNumber: number): number => {
