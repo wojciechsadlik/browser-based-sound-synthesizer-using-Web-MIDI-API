4296825bd04122bee3a395f26cf221f42fa18b08
wojciechsadlik <wojciech.sadlik.dev@gmail.com>
2020-10-29 11:40:39 +0100

    add waveform button, waveform selectors

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,15 @@ import SoundGenerator from './SoundGenerator';
 
 const midiInputSelectElem = document.getElementById('MIDI_Input_sel') as HTMLSelectElement;
 const volumeElem = document.getElementById('volume') as HTMLInputElement;
-const waveformSelectElem = document.getElementById('waveform_sel') as HTMLSelectElement;
+const addWaveformBtnElem = document.getElementById('add_waveform_btn') as HTMLButtonElement;
+const waveformsDivElem = document.getElementById('waveforms') as HTMLDivElement;
+// const waveformSelectElem = document.getElementById('waveform_sel') as HTMLSelectElement;
 
 const audioContext = new window.AudioContext();
 const soundGenerator = new SoundGenerator(audioContext);
 soundGenerator.setVolume(Number(volumeElem.value));
-soundGenerator.setWaveType(waveformSelectElem.value as OscillatorType);
-waveformSelectElem.addEventListener('change', waveformChange);
+// soundGenerator.setWaveType(waveformSelectElem.value as OscillatorType);
+// waveformSelectElem.addEventListener('change', waveformChange);
 
 window.addEventListener('click', resumeAudioContext);
 
@@ -21,22 +23,62 @@ midiInputSelectElem.addEventListener('change', inputSelectChange);
 
 volumeElem.addEventListener('change', volumeChange);
 
-function inputSelectChange(e: Event) {
+addWaveformBtnElem.addEventListener('click', addWaveform);
+
+function inputSelectChange() {
     midiCommunicator.setSelectedInput(midiInputSelectElem.value);
 }
 
-function volumeChange(e: Event) {
+function volumeChange() {
     soundGenerator.setVolume(Number(volumeElem.value));
 }
 
-function waveformChange(e: Event) {
-    soundGenerator.setWaveType(waveformSelectElem.value as OscillatorType);
+function waveformChange() {
+    // soundGenerator.setWaveType(waveformSelectElem.value as OscillatorType);
 }
 
-function resumeAudioContext(e: MouseEvent) {
+function resumeAudioContext() {
     audioContext.resume().then(() => {
         console.log('Audio context resumed successfully');
         
         window.removeEventListener('click', resumeAudioContext);
     });
+}
+
+function addWaveform() {
+    waveformsDivElem.appendChild(createWaveformSelector(waveformsDivElem.childElementCount));
+}
+
+function removeWaveform(this: HTMLButtonElement) {
+    waveformsDivElem.removeChild(this.parentNode as Node);
+}
+
+function createWaveformSelector(id: number): HTMLDivElement {
+    let selectorDiv = document.createElement('div');
+
+    let selector = document.createElement('select');
+    addWaveforms(selector);
+
+    let removeBtn = document.createElement('button');
+    removeBtn.appendChild(document.createTextNode('Remove'));
+    removeBtn.addEventListener('click', removeWaveform);
+    
+    selectorDiv.appendChild(selector);
+    selectorDiv.appendChild(removeBtn);
+
+    return selectorDiv;
+}
+
+function addWaveforms(selector: HTMLSelectElement) {
+    let waveforms = ['square',
+        'sawtooth',
+        'triangle',
+        'sine']
+
+    for (let waveform of waveforms) {
+        let option = document.createElement('option');
+        option.text = waveform;
+        option.value = waveform;
+        selector.add(option);
+    }
 }
